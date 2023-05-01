c53334eea5985941912f889d2780b713ef7735a1
12e21 <2496813401@qq.com>
2023-05-01 19:30:03 +0800

    完成了单一元分类程序

--- project/logistic_regression_project/logistic_regression.py
+++ project/logistic_regression_project/logistic_regression.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib as plt
 import utils
-
+'''
+本程序为
+'''
 
 
 def hypothetical(theta:np.ndarray,x:np.ndarray)->np.ndarray:
@@ -32,17 +34,59 @@ def cal_loss(theta:np.ndarray,x:np.ndarray,y:np.ndarray):
     return result
 
 
+def cal_gradient(theta:np.ndarray,x:np.ndarray,y:np.ndarray)->np.ndarray:
+    '''
+    梯度函数
+    theta: 参数向量
+    x: 特征矩阵
+    y: 标签矩阵
+    返回: 梯度向量
+    '''
+    predict=hypothetical(theta,x)
+    diff=predict-y
+    middle=np.dot(x.T,diff)
+    return middle/x.shape[0]
+
+
 
 if __name__ == "__main__":
-    x=np.array([[1,20,0.3],
-                [1,22,0.1],
-                [1,18,0.4],
-                [1,17,0.6],
-                [1,19,0.5],
-                [1,21,0.2],
-                [1,23,0.1]])
+    x=np.array([[1,11,0.91],
+                [1,12,0.88],
+                [1,14,0.72],
+                [1,20,0.63],
+                [1,24,0.51],
+                [1,28,0.42],
+                [1,29,0.33]])
     
-    y=np.array([1,1,1,0,1,0,1])
+    x[:,1:]=utils.normalize_matrix(x[:,1:])
+    y=np.array([1,1,1,0,0,0,0])
     theta=np.array([0.1,0.2,0.4])
-    print(hypothetical(theta,x))
-    print(cal_loss(theta,x,y))
+
+    #设置迭代次数计数器,误差,学习率
+    iter_counter=0
+    loss=3
+    pre_loss=1
+    learning_rate=0.0001
+
+    #开始迭代直至到达最大循环次数或收敛
+    while not(iter_counter==1000000 or abs(loss-pre_loss) < 0.00000003):
+        # 计算梯度
+        gradients=cal_gradient(theta,x,y)
+
+        # 更新梯度
+        theta=theta-learning_rate*gradients
+
+        # 计算误差
+        pre_loss=loss
+        loss=cal_loss(theta,x,y)
+        
+        # 打印误差
+        print(loss)
+
+    # 打印最后参数
+    print(theta)
+    # 预测结果
+    result=hypothetical(theta,x)
+    result[np.where(result>0.50000)]=1
+    result[np.where(result<0.50000)]=0
+    print(result)
