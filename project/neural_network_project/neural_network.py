88eb9a6cedc5e963757bf4653c312b3236825cc6
12e21 <2496813401@qq.com>
2023-05-21 22:39:42 +0800

    add gradient check part

--- project/neural_network_project/neural_network.py
+++ project/neural_network_project/neural_network.py
@@ -22,11 +22,12 @@ layer=list(range(layer_count))
 z=list(range(layer_count))
 a=list(range(layer_count+1))
 # 设置梯度
-gradients=[np.zeros([4,2]),np.zeros([4,4]),np.zeros([3,4]),np.zeros([2,3])]
+gradients=[np.zeros([4,3]),np.zeros([4,5]),np.zeros([3,5]),np.zeros([2,4])]
 
 
+counter=0
 # 迭代
-while(True):
+while(counter<1):
     # 前向传播
     a[0]=feature.T
     for i in range(0,layer_count):
@@ -60,11 +61,49 @@ while(True):
     for i in range(len(gradients)):
         gradients[i]=(1.0/y.shape[1])*np.dot(errors[i],layer[i].T)+regular_rate*params[i]
 
-
+    
+    # 梯度检测
+    epsilon=0.001
+    approx_params=params.copy()
+    approx_gradients=[np.zeros([4,3]),np.zeros([4,5]),np.zeros([3,5]),np.zeros([2,4])]
+    for i in range(len(approx_params)):
+        for j in range(approx_params[i].shape[0]):
+            for k in range(approx_params[i].shape[1]):
+                
+                approx_params[i][j,k]+=epsilon
+                
+                # 前向传播
+                a[0]=feature.T
+                for m in range(0,layer_count):
+                    # 添加bias
+                    layer[m]=np.concatenate((a[m],np.ones([1,a[m].shape[1]])),axis=0)
+                    # 通过权重
+                    z[m]=np.dot(approx_params[m],layer[m])
+                    # 进入激活函数
+                    a[m+1]=sigmoid(z[m])
+                
+                # 计算误差
+                y=label
+                hypo_x:np.ndarray = a[4]
+                regular_rate=0.001
+                # 误差计算公式
+                approx_loss=(-1./y.shape[1])*((y*np.log(hypo_x)+(1-y)*np.log(1-hypo_x)).flatten().sum()) + (regular_rate/(2*y.shape[0]))*sum([param.flatten().sum() for param in approx_params])
+                approx_gradients[i][j,k]=(approx_loss-loss)/epsilon
+                
+    
     # 更新参数
-    learning_rate=0.002
+    learning_rate=0.3
     for i in range(len(params)):
         params[i]=params[i]-learning_rate*gradients[i]
+
+    # 比较梯度
+    
+    print(gradients)
+    print("!!!!!!!!!!!!!!!!!!")
+    print(approx_gradients)
+    
+
+    counter+=1
 
     
 
--- test/numpy_test.py
+++ test/numpy_test.py
@@ -3,3 +3,8 @@ x = np.linspace(1, 100, 5) # 生成1到100的等差数列
 y = np.linspace(1, 100, 5) # 生成1到100的等差数列
 X, Y = np.meshgrid(x, y) # 生成网格点坐标矩阵
 Z = np.vstack((X.ravel(), Y.ravel())).T # 把坐标矩阵转换成每一行是一对xy的形式
+
+
+x=np.ones([3,2])
+for i in range(3):
+    print(i)
